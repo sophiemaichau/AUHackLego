e8d19e79ea22b2c2cd4603062f94d5b99218440e
sophiemaichau <sophie_chau@hotmail.com>
2018-04-14 14:43:49 +0200

    Shape detection works

--- showcam.py
+++ showcam.py
@@ -6,6 +6,12 @@ By Jacob Schnedler, Mikkel Milo and Sophie Mai Chau
 import numpy as np
 import cv2
 import imutils
+from matplotlib import pyplot as plt
+from skimage import feature
+from skimage import measure
+from skimage import io
+from skimage.io import imsave
+from detect_shapes import detect
 
 def start_video_stream():
     cap = cv2.VideoCapture(0)
@@ -15,13 +21,13 @@ def start_video_stream():
         ret, frame = cap.read()
 
         # Our operations on the frame come here
-        #gray = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2GRAY)  # making the image grey and flipping it vertically
-        #gray = imutils.resize(gray, width=400)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        #cv2.putText(gray, 'Lego', (10, 500), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+        cimg = cv2.flip(frame, 1) # flipping it vertically
+        gray = cv2.cvtColor(cimg, cv2.COLOR_BGR2GRAY)  # making the image grey
+        hsv = cv2.cvtColor(cimg, cv2.COLOR_BGR2HSV)
+        cimg = detect(cimg, hsv)
 
         # Display the resulting frame
-        cv2.imshow('AUHack LegoMusic', cv2.flip(frame, 1))
+        cv2.imshow('AUHack LegoMusic', cimg)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -30,4 +36,11 @@ def start_video_stream():
     cap.release()
     cv2.destroyAllWindows()
 
+# for testing
+def draw_edges(im_path):
+    image = cv2.imread(im_path)
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    detect(image, hsv)
+
 start_video_stream()
+#draw_edges('lego.jpg')
